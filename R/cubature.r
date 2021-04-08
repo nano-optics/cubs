9d25f07c847c1a31ac5599ec24e83b847a5fa49c
baptiste auguie <baptiste.auguie@gmail.com>
2021-04-08 17:58:42 +1200

    test yl20m10

--- R/cubature.r
+++ R/cubature.r
@@ -7,7 +7,7 @@
 ##'
 ##' Numerical integration points for angular averaging
 ##' @title cubature_sphere
-##' @param Nq number of integration points
+##' @param N number of integration points
 ##' @param cubature cubature method, using either Gauss Legendre cubature (default), Quasi Monte Carlo, regular grid, or "cheap" (3 axes)
 ##' @param init (qmc method only) logical: restart, or continue from previous call
 ##' @importFrom randtoolbox halton
@@ -22,14 +22,12 @@ cubs <- function(N = 30,
   cubature <- match.arg(cubature)
   
   if(cubature == "lebedev"){
-  # lebedev_table <- readRDS('/Users/auguieba/Documents/nano-optics/cubs/data/lebedev_table.rds')
     if(N > max(lebedev_table$N)) w <- nrow(lebedev_table) else
     w <- which(lebedev_table$N >= 36)[1]
     return(lebedev[[w]])
   }
   
   if(cubature == "sphericaldesigns"){
-    # sphericaldesigns_table <- readRDS('/Users/auguieba/Documents/nano-optics/cubs/data/sphericaldesigns_table.rds')
     if(N > max(sphericaldesigns_table$N)) w <- nrow(sphericaldesigns_table) else
       w <- which(sphericaldesigns_table$N >= 36)[1]
     return(sphericaldesigns[[w]])
@@ -38,14 +36,16 @@ cubs <- function(N = 30,
   
   if(cubature == "gl"){ #  gauss legendre along cos beta, grid alpha
     # might have slightly more than N total points
-    # N = 2P^2 -> P = [sqrt(N/2)]
+    # N = (P+1)*(P+1)/2 = (P+1)^2/2 -> P = [sqrt(2*N) - 1]
+    P <- sqrt(2*N) - 1
     # now check +/-1 on each one and pick best combo
-    P <- sqrt(N/2)
-    opts <- expand.grid(N1 = round(P) + seq(-1,1), N2=2*round(P) + seq(-1,1))
-    prod <- opts$N1 * opts$N2
-    best <- which.min(abs(N - prod))
-    Nbeta <- opts$N1[best]
-    Nalpha <- opts$N2[best]
+    # opts <- expand.grid(N1 = round(P) + seq(-1,1), N2=2*round(P) + seq(-1,1))
+    # prod <- opts$N1 * opts$N2
+    # best <- which.min(abs(N - prod))
+    # Nbeta <- opts$N1[best]
+    # Nalpha <- opts$N2[best]
+    Nalpha <- ceiling(P+1)
+    Nbeta <- ceiling((P+1)/2)
     
     alpha <- seq(0, 2*pi *(1 - 1/Nalpha), by = 2*pi/Nalpha)
     GL_cbeta <- statmod::gauss.quad(Nbeta)
@@ -56,7 +56,8 @@ cubs <- function(N = 30,
     # corresponding weights for 2D cubature
     weights <- expand.grid(alpha=rep(1/Nalpha, Nalpha),
                            beta=GL_cbeta$weights)
-    # combine the weights and divide by 2 (divide by 4pi but multiply by 2pi)
+    # combine the weights and divide by 2 
+    # (1/4pi for the average, but * 2pi from range of alpha)
     weights <- 1/2 * weights$alpha * weights$beta
     
     return(cbind(as.matrix(nodes), weights))
@@ -64,74 +65,53 @@ cubs <- function(N = 30,
   
   if(cubature == "qmc"){ # quasi monte-carlo
     
-    p <- randtoolbox::halton(Nq, dim = 2, normal=FALSE, init=init)
+    p <- randtoolbox::halton(N, dim = 2, normal=FALSE, init=TRUE)
     
     alpha <- p[,1]*2*pi
     beta <- acos(2*p[,2] - 1) # cos(beta) in [-1,1]
-    nodes <- rbind(alpha=alpha, beta=beta, gamma=0)
-    weights <- rep(1/ncol(nodes), ncol(nodes))
-    return(list(nodes=nodes, weights=weights))
+    nodes <- cbind(alpha=alpha, beta=beta)
+    weights <- rep(1/nrow(nodes), nrow(nodes))
+    return(cbind(as.matrix(nodes), weights))
   }
   
   if(cubature == "random"){ # monte-carlo with random points
     
-    alpha <- runif(Nq, 0, 2*pi) # uniform [-pi,pi]
-    beta <- acos(runif(Nq, -1, 1)) # cos-uniform [-1,1]
-    nodes <- rbind(alpha=alpha, beta=beta, gamma=0)
-    weights <- rep(1/ncol(nodes), ncol(nodes))
-    return(list(nodes=nodes, weights=weights))
+    alpha <- runif(N, 0, 2*pi) # uniform [-pi,pi]
+    beta <- acos(runif(N, -1, 1)) # cos-uniform [-1,1]
+    nodes <- cbind(alpha=alpha, beta=beta)
+    weights <- rep(1/nrow(nodes), nrow(nodes))
+    return(cbind(as.matrix(nodes), weights))
   }
   
   if(cubature == "grid"){ # grid in acos beta and alpha
     # might have slightly more than N total points
     # N = 2P^2 -> P = [sqrt(N/2)]
     # now check +/-1 on each one and pick best combo
-    P <- sqrt(Nq/2)
-    opts <- expand.grid(N1 = round(P) + seq(-1,1), N2=2*round(P) + seq(-1,1))
+    P <- sqrt(N/2)
+    opts <- expand.grid(N1 = round(P) + seq(-1,1), 
+                        N2=2*round(P) + seq(-1,1))
     prod <- opts$N1 * opts$N2
-    best <- which.min(abs(Nq - (prod+2))) # add two poles
+    best <- which.min(abs(N - prod)) 
     Nbeta <- opts$N1[best]
     Nalpha <- opts$N2[best]
     
     alpha <- seq(0, 2*pi *(1 - 1/Nalpha), by = 2*pi/Nalpha)
-    beta <- acos(seq(-1+1/(2*Nbeta+1),1-1/(2*Nbeta+1), length.out = Nbeta)) 
+    beta <- acos(seq(-1+1/(2*Nbeta+1), 1-1/(2*Nbeta+1), 
+                     length.out = Nbeta)) 
     # cos-uniform ]-1,1[ exclude poles as otherwise many points there
     # nodes <- rbind(expand.grid(alpha=alpha, beta=beta, gamma=0),
     #                c(0,0,0), c(0, pi, 0)) # add two poles
-    nodes <- expand.grid(alpha=alpha, beta=beta, gamma=0)
+    nodes <- expand.grid(alpha=alpha, beta=beta)
     weights <- rep(1/nrow(nodes), nrow(nodes))
     
-    return(list(nodes=t(nodes), weights=weights))
+    return(cbind(as.matrix(nodes), weights))
   }
-  if(cubature == "grid2"){ # grid in acos beta and alpha
-    # might have slightly more than N total points
-    # N = 2P^2 -> P = [sqrt(N/2)]
-    # now check +/-1 on each one and pick best combo
-    P <- sqrt(Nq/2)
-    opts <- expand.grid(N1 = round(P) + seq(-1,1), N2=2*round(P) + seq(-1,1))
-    prod <- opts$N1 * opts$N2
-    best <- which.min(abs(Nq - (prod+2))) # add two poles
-    Nbeta <- opts$N1[best]
-    Nalpha <- opts$N2[best]
-    
-    alpha <- seq(0, 2*pi *(1 - 1/Nalpha), by = 2*pi/Nalpha)
-    # beta <- acos(seq(-1+1/(2*Nbeta+1),1-1/(2*Nbeta+1), length.out = Nbeta)) 
-    step <- 2/Nbeta
-    beta <- acos(seq(-1+step,1-step, length.out = Nbeta)) 
-    # cos-uniform ]-1,1[ exclude poles as otherwise many points there
-    # nodes <- rbind(expand.grid(alpha=alpha, beta=beta, gamma=0),
-    #                c(0,0,0), c(0, pi, 0)) # add two poles
-    nodes <- expand.grid(alpha=alpha, beta=beta, gamma=0)
-    weights <- rep(1/nrow(nodes), nrow(nodes))
-    
-    return(list(nodes=t(nodes), weights=weights))
-  }
-  
+
   
   if(cubature == "fibonacci"){ # spiral
     
-    N0 <- Nq
-    if(Nq%%2 == 1) N0 <- Nq+1
+    N0 <- N
+    if(N%%2 == 1) N0 <- N+1
     P <- (N0-1)/2
     ii <- seq(-P,P,by=1)
     # asin(2*ii/N0) latitude = asin(x) -> colatitude = pi/2 - asin(x)
@@ -139,16 +119,16 @@ cubs <- function(N = 30,
     golden <- (1+sqrt(5))/2
     alpha <- (2*pi*ii/golden) %% (2*pi)
     
-    nodes <- rbind(alpha=alpha, beta=beta, gamma=0)
-    weights <- rep(1/ncol(nodes), ncol(nodes))
-    return(list(nodes=nodes, weights=weights))
+    nodes <- cbind(alpha=alpha, beta=beta)
+    weights <- rep(1/nrow(nodes), nrow(nodes))
+    return(cbind(as.matrix(nodes), weights))
   }
   
   
   if(cubature == "fibonacci2"){ # spiral
     
-    N0 <- Nq
-    if(Nq%%2 == 1) N0 <- Nq+1
+    N0 <- N
+    if(N%%2 == 1) N0 <- N+1
     
     ii <- seq(0,N0,by=1)
     
@@ -156,9 +136,9 @@ cubs <- function(N = 30,
     golden <- (1+sqrt(5))/2
     alpha <- (2*pi*ii/golden) %% (2*pi)
     
-    nodes <- rbind(alpha=alpha, beta=beta, gamma=0)
+    nodes <- rbind(alpha=alpha, beta=beta)
     weights <- rep(1/ncol(nodes), ncol(nodes))
-    return(list(nodes=nodes, weights=weights))
+    return(cbind(as.matrix(nodes), weights))
   }
   
   
--- tests/test_function.R
+++ tests/test_function.R
@@ -4,21 +4,98 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 
+
+f0 <- function(phi, theta){
+  
+  # https://math.stackexchange.com/a/1847806/4964
+  
+  # https://www.wolframalpha.com/input/?i=+SphericalHarmonicY%5B35%2C27%2C%CE%B8%2C%CF%95%5D+
+  
+  # https://www.wolframalpha.com/input/?i=Integrate+sin%28theta%29*+SphericalHarmonicY%5B12%2C-7%2C%CE%B8%2C%CF%95%5D++from+theta%3D0%2C+phi%3D0+to+theta%3Dpi%2Cphi%3D2*pi
+  
+  yl35m27 = Re(-exp(27i*phi)*sin(theta)^27*(180297*cos(theta)^8 - 73164*cos(theta)^6 + 8190*cos(theta)^4 - 252*cos(theta)^2 + 1))
+  
+  yl12mm7 = Re((exp(-7i* phi)* sin(theta)^7* cos(theta)* (161 *cos(theta)^4 - 70* cos(theta)^2 + 5)))
+  
+  yl12m0 = Re(5 *(676039*  cos(theta)^12 - 1939938 * cos(theta)^10 + 2078505 * cos(theta)^8 - 1021020 * cos(theta)^6 + 225225 * cos(theta)^4 - 18018 * cos(theta)^2 + 231)/2000)
+  
+  yl1m1 <- Re(exp(1i*phi)*sin(theta))
+  yl2m1 <- Re(exp(1i*phi)*sin(theta)*cos(theta))
+  yl3m1 <- Re(exp(1i*phi)*sin(theta)*(5*cos(theta)^2 - 1))
+  yl5m1 <-  Re(exp(1i*phi)*sin(theta)*(21* cos(theta)^4 - 14 *cos(theta)^2 + 1))
+  yl5m3 <- Re(exp(3i*phi)* sin(theta)^3 * (9 * cos(theta)^2 - 1))
+  
+  yl8m5 <- Re( exp(5i*phi)* sin(theta)^5 * cos(theta)* (5 * cos(theta)^2 - 1))
+  yl15m15 <- Re( exp(15i*phi)* sin(theta)^15)
+  yl20m20 <- Re( exp(20i*phi)* sin(theta)^20)
+  yl30m30 <- Re( exp(30i*phi)* sin(theta)^30)
+  
+  yl20m13 <- -Re( (15 *sqrt(63253693041/(2*pi)) * exp(13i*phi) *sin(theta)^13 *cos(theta) * (481*cos(theta)^6 - 259*cos(theta)^4 + 35*cos(theta)^2 - 1))/262144)
+  yl12mm7
+  yl12m0
+  yl20m20
+  yl35m27
+  # yl20m13
+  yl12m0
+  yl8m5
+  yl2m2 <- Re(1/4*sqrt(15/(2*pi))*exp(2i*phi)*sin(theta)^2)
+  yl2m2
+  yl5m2 <- Re(1/8*sqrt(1155/(2*pi))*exp(2i*phi)*sin(theta)^2* cos(theta)*(3*cos(theta)^2 - 1))
+  yl5m2
+  yl15m15 
+  Re(exp(12i*phi)*sin(theta)^12*cos(theta)*(29*cos(theta)^2 - 3))
+  Re(exp(7i*phi)*sin(theta)^7* (30015*cos(theta)^8 - 28980 *cos(theta)^6 + 8050 *cos(theta)^4 - 644*cos(theta)^2 + 7))
+  yl12mm7
+  yl20m20
+  yl19m17 <- Re(exp(17i*phi)*sin(theta)^17*(37*cos(theta)^2 - 1))
+  yl19m17
+  #  SphericalHarmonicY[20,10,θ,ϕ] 
+  Re(exp(10i*phi)*sin(theta)^10*(164021*cos(theta)^10 - 189255*cos(theta)^8 + 71610*cos(theta)^6 - 10230*cos(theta)^4 + 465*cos(theta)^2 - 3))
+}
+
+rotate_x <- function(phi, theta, alpha){
+  
+  Rx = rbind(c(cos(alpha/2),-1i*sin(alpha/2)),
+             c(-1i*sin(alpha/2),cos(alpha/2)))
+  
+  Q <- rbind(cos(theta/2), exp(1i*phi)*sin(theta/2))
+  
+  RQ <- Rx %*% Q
+  
+  thetap <- apply(RQ, 2, function(z) 2*atan2(Mod(z[2]),Mod(z[1])))
+  phip <- apply(RQ, 2, function(z) Arg(z[2]) - Arg(z[1]))
+  
+  data.frame(phi = phip, theta = thetap)
+}
+
+pt <- expand.grid(phi = seq(-pi,pi,length=10),
+                  theta = seq(0,pi,length=10))
+
+pt2 <- rotate_x(pt$phi, pt$theta, alpha=10*pi/180)
+# 
+# ggplot(pt, aes(phi,theta)) +
+#   geom_point()+
+#   geom_point(data=pt2,colour='red',alpha=0.5)
+
+
 g <- expand.grid(phi=seq(0,2*pi,length=360), theta=seq(0,pi,length=180))
 
+g2 <- rotate_x(g$phi-pi, g$theta, alpha=-30*pi/180)
+
+g$f0 <- f0(g$phi,g$theta)
+g$f00 <- f0(g2$phi,g2$theta)
 g$f1 <- f1(g$phi,g$theta)
 g$f2 <- f2(g$phi,g$theta)
 g$f3 <- f3(g$phi,g$theta)
 
-
-gm <- g %>% pivot_longer(c('f1','f2','f3')) %>% 
+gm <- g %>% pivot_longer(c('f00','f0','f1','f2','f3')) %>% 
   group_by(name) %>% 
   mutate(normalised = scales::rescale(value))
 
 str(gm)
 
-
-ggplot(gm, aes(phi, theta, fill=normalised)) +
+p0 <- 
+  ggplot(gm, aes(phi, theta, fill=normalised)) +
   facet_wrap(~name,ncol=1)+
   geom_raster() +
   coord_equal()+
@@ -26,11 +103,68 @@ ggplot(gm, aes(phi, theta, fill=normalised)) +
   scale_x_continuous(expand=c(0,0)) +
   scale_y_continuous(expand=c(0,0)) 
 
+p0
+
+rg <- ggplot(gm %>% filter(name=='f0'), aes(phi, theta, fill=normalised)) +
+  # facet_wrap(~name,ncol=1)+
+  geom_raster() +
+  coord_equal()+
+  scale_fill_distiller(palette = 'PiYG') +
+  theme_void()+ theme(legend.position = 'none')+
+  scale_x_continuous(expand=c(0,0)) +
+  scale_y_continuous(expand=c(0,0)) 
+
+ggsave('f0.png',rg, width=6,height=3)
+
+rg <- ggplot(gm %>% filter(name=='f00'), aes(phi, theta, fill=normalised)) +
+  # facet_wrap(~name,ncol=1)+
+  geom_raster() +
+  scale_fill_distiller(palette = 'PiYG') +
+  coord_equal()+
+  theme_void()+ theme(legend.position = 'none')+
+  scale_x_continuous(expand=c(0,0)) +
+  scale_y_continuous(expand=c(0,0)) 
+
+ggsave('f00.png',rg, width=6,height=3)
+
+library(rgl)
+open3d()
+texture <- "f0.png"
+
+lat <- matrix(seq(90, -90, len = 50)*pi/180, 50, 50, byrow = TRUE)
+long <- matrix(seq(-180, 180, len = 50)*pi/180, 50, 50)
+
+r <- 1
+x <- r*cos(lat)*cos(long)
+y <- r*cos(lat)*sin(long)
+z <- r*sin(lat)
+
+
+open3d()
+texture <- "f0.png"
+texture2 <- "f00.png"
+
+persp3d(x, y, z, col = "white", 
+        texture = texture, alpha=1,
+        specular = "black", axes = F, box = F, xlab = "", ylab = "", zlab = "",
+        normal_x = x, normal_y = y, normal_z = z)
+
+
+persp3d(x+2*max(x), y, z, col = "white", 
+        texture = texture2, alpha=1,
+        specular = "black", axes = F, box = F, xlab = "", ylab = "", zlab = "",
+        normal_x = x, normal_y = y, normal_z = z, add = TRUE)
+
 library(cubature)
 
+I0 <- 0
 I1 <- 216*pi/35
 I2 <- 6.6961822200736179523
 I3 <- 4*pi/9
+
+# num <- hcubature(function(x) matrix(sin(x[2,]) * f0(x[1,],x[2,])), 
+#                  c(0,0), c(2*pi, pi), vectorInterface = TRUE)
+# num$integral
 
 num <- hcubature(function(x) sin(x[2]) * f1(x[1],x[2]) , c(0,0), c(2*pi, pi))
 num$integral
@@ -47,16 +181,24 @@ I3
 
 test_quadrature <- function(quad){
   
+  q2 <- rotate_x(quad[,1]-pi, quad[,2], alpha=-30*pi/180)
+  
+  vals0 <- f0(quad[,1], quad[,2])
+  vals00 <- f0(q2[,1], q2[,2])
   vals1 <- f1(quad[,1], quad[,2])
   vals2 <- f2(quad[,1], quad[,2])
   vals3 <- f3(quad[,1], quad[,2])
   
   d <- data.frame(N = nrow(quad),
+                  sum0 = sum(vals0 * quad[,3]),
+                  sum00 = sum(vals00 * quad[,3]),
                   sum1 = sum(vals1 * quad[,3]),
                   sum2 = sum(vals2 * quad[,3]),
                   sum3 = sum(vals3 * quad[,3]))
   
   mutate(d,
+         err00 = sum00 - I0/(4*pi),
+         err0 = sum0 - I0/(4*pi),
          err1 = sum1 - I1/(4*pi),
          err2 = sum2 - I2/(4*pi),
          err3 = sum3 - I3/(4*pi))
@@ -68,33 +210,46 @@ data("sphericaldesigns")
 res1 <- map_df(lebedev, test_quadrature)
 res2 <- map_df(sphericaldesigns, test_quadrature)
 
-gausslegendre <- lapply(5:150, cubs, cubature = 'gl')
+gausslegendre <- lapply(c(5:150,seq(200,1e4,by=100)), cubs, cubature = 'gl')
 res3 <- map_df(gausslegendre, test_quadrature)
 
+fibonacci <- lapply(c(5:150,seq(200,1e4,by=100)), 
+                    cubs, cubature = 'fibonacci')
+res4 <- map_df(fibonacci, test_quadrature)
+
+qmc <- lapply(c(5:150,seq(200,1e4,by=100)), cubs, cubature = 'qmc')
+res5 <- map_df(qmc, test_quadrature)
 
 
 # str(res)
 
 resm1 <- res1 %>% mutate(quad = 'lebedev') %>% 
-  pivot_longer(c('err1','err2','err3'))
+  pivot_longer(c('err00','err0','err1','err2','err3'))
 
 resm2 <- res2 %>% mutate(quad = 'sphericaldesigns') %>% 
-  pivot_longer(c('err1','err2','err3'))
+  pivot_longer(c('err00','err0','err1','err2','err3'))
 
 resm3 <- res3 %>% mutate(quad = 'gausslegendre') %>% 
-  pivot_longer(c('err1','err2','err3'))
+  pivot_longer(c('err00','err0','err1','err2','err3'))
 
+resm4 <- res4 %>% mutate(quad = 'fibonacci') %>% 
+  pivot_longer(c('err00','err0','err1','err2','err3'))
 
-resm <- rbind(resm1,resm2,resm3)
-p <- ggplot(subset(resm, N > 1 & N < 250 & !(quad %in% c('grid2','grid','qmc'))),
+resm5 <- res5 %>% mutate(quad = 'qmc') %>% 
+  pivot_longer(c('err00','err0','err1','err2','err3'))
+
+resm <- rbind(resm1,resm2,resm3,resm4,resm5) 
+resm$value[abs(resm$value)<1e-16] <- 1e-16
+
+p <- ggplot(subset(resm, N > 1 & N < 25000 & !(quad %in% c('grid2','grid'))),
             aes(N, abs(value),colour=quad)) +
-  facet_grid(name~.,scales = 'fixed')+
+  facet_wrap(~name,scales = 'free_y',ncol=1)+
   geom_line() +
   scale_x_log10() +
-  scale_y_log10() +
+  scale_y_log10(lim=10^c(-16,0)) +
   theme() +
   labs(x=expression(N[nodes]),y='abs(error)',colour='')+
   theme()
 
-p
+egg::ggarrange(p0+coord_cartesian()+theme(legend.position = 'none'),p,ncol=2)
 
